2e308312334f3f45ebefaad78b4b3169dbc779a7
Jacob <41574808+cheng0210@users.noreply.github.com>
2024-07-18 00:41:01 +1000

    Update run_quectel.sh

--- run_quectel.sh
+++ run_quectel.sh
@@ -90,7 +90,7 @@ teardown_network() {
     ip link set $INTERFACE down >> "$LOG_FILE" 2>&1
 
     echo "$(date +'%Y-%m-%d %H:%M:%S') - Restarting Quectel module..." >> "$LOG_FILE"
-    echo "AT+CFUN=1,1" | socat - /dev/ttyUSB2,crnl
+    echo -n -e "AT+CFUN=1,1\r\n" > /dev/ttyUSB2 && cat /dev/ttyUSB2
     sleep 60
     echo "$(date +'%Y-%m-%d %H:%M:%S') - Quectel restarted"
 }
