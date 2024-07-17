1b792994e3eb745f448bbe2ea6be47ad410e1668
Jacob <41574808+cheng0210@users.noreply.github.com>
2024-07-17 18:20:56 +1000

    Update stop_quectel.sh

--- stop_quectel.sh
+++ stop_quectel.sh
@@ -17,3 +17,33 @@ for pid in $pids; do
     echo "Killing process $pid"
     kill $pid
 done
+
+# Environment variables for network configuration
+INTERFACE=${INTERFACE:-wwan0}
+DNN0=oai
+#DNN1=ims
+DNN_TYPE0=${DNN_TYPE0:-ipv4}
+#DNN_TYPE1=${DNN_TYPE1:-ipv4v6}
+DEVICE=/dev/cdc-wdm*
+LOG_FILE="quectel_connection.log"  # Path to the log file
+
+# Function to tear down network sessions
+teardown_network() {
+    echo "$(date +'%Y-%m-%d %H:%M:%S') - mbimcli -d ${DEVICE} -p --disconnect=0" >> "$LOG_FILE"
+    mbimcli -d $DEVICE -p --disconnect=0 >> "$LOG_FILE" 2>&1
+    if [[ -v DNN1 ]]; then
+        echo "$(date +'%Y-%m-%d %H:%M:%S') - mbimcli -d ${DEVICE} -p --disconnect=1" >> "$LOG_FILE"
+        mbimcli -d $DEVICE -p --disconnect=1 >> "$LOG_FILE" 2>&1
+        echo "$(date +'%Y-%m-%d %H:%M:%S') - ip link set ${INTERFACE}.1 down" >> "$LOG_FILE"
+        ip link set $INTERFACE.1 down >> "$LOG_FILE" 2>&1
+        echo "$(date +'%Y-%m-%d %H:%M:%S') - ip link del link wwan0 name ${INTERFACE}.1 type vlan id 1" >> "$LOG_FILE"
+        ip link del link wwan0 name $INTERFACE.1 type vlan id 1 >> "$LOG_FILE" 2>&1
+    fi
+    echo "$(date +'%Y-%m-%d %H:%M:%S') - mbimcli -p -d ${DEVICE} --set-radio-state=off" >> "$LOG_FILE"
+    mbimcli -p -d $DEVICE --set-radio-state=off >> "$LOG_FILE" 2>&1
+
+    echo "$(date +'%Y-%m-%d %H:%M:%S') - Removing the $INTERFACE" >> "$LOG_FILE"
+    ip link set $INTERFACE down >> "$LOG_FILE" 2>&1
+}
+
+teardown_network
