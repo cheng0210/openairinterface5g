d991c6d6a52dd4319518d081985c995dcf726712
Cheng Jiang <cheng.jiang0210@gmail.com>
2024-07-25 17:09:39 +1000

    add ping timeout

--- run_quectel.py
+++ run_quectel.py
@@ -12,19 +12,18 @@ def send_at_command(command):
     with serial.Serial(serial_port, baud_rate, timeout=1) as ser:
         ser.write(command.encode('utf-8'))
         response = ser.readline().decode('utf-8').strip()
-        assert command == response
         response = ser.readline().decode('utf-8').strip()
+        # Response should be OK
         print(f"Sent command: {command}, Response: {response}")
 
 # Ping test function
 def check_internet_connection(ping_count):
     host = "8.8.8.8"  # Google's public DNS server
     for _ in range(ping_count):
-        result = subprocess.run(['ping','-I', 'usb0' ,'-c', '1', host], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        result = subprocess.run(['ping','-I', 'usb0' ,'-c', '1', '-W', '0.5', host], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
         if result.returncode == 0:
             print(datetime.datetime.now().isoformat() + ": " + "Ping Success")
             return True
-        time.sleep(0.1)
     print(datetime.datetime.now().isoformat() + ": " + "Ping failed {ping_count} times")
     return False
 
